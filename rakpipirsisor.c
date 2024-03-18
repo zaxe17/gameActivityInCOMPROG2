7a0ba0a4f36402c986f3adf53ee83cb516c55fc2
Zaxe <119933185+zaxe17@users.noreply.github.com>
2024-03-18 20:11:43 +0800

    Update and rename main.c to rakpipirsisor.c

---
+++ rakpipirsisor.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+void winner(int choice1, int choice2) {
+    if(choice1 == 6 && choice2 == 2) {
+        printf("Player 1 wins this round\n");
+    }
+    else if(choice1 == 2 && choice2 == 6) {
+        printf("Player 2 wins this round\n");
+    }
+    else if(choice1 < choice2) {
+        printf("Player 1 wins this round\n");
+    }
+    else if(choice1 > choice2) {
+        printf("Player 2 wins this round\n");
+    }
+    else {
+        printf("Tie in this round\n");
+    }
+}
+
+void table(int array[4][2], int round) {
+    printf("\nRound %d:\n", round + 1);
+
+    for(int row = 0; row <= round; row++) {
+        for(int column = 0; column < 2; column++) {
+            printf("%d ", array[row][column]);
+        }
+        
+        int result = 0;
+
+        if (array[row][0] == 6 && array[row][1] == 2) {
+            result = 1;
+        }
+        else if (array[row][0] == 2 && array[row][1] == 6) {
+            result = -1;
+        }
+        else if (array[row][0] < array[row][1]) {
+            result = 1;
+        }
+        else if (array[row][0] > array[row][1]) {
+            result = -1;
+        }
+        
+        printf("| %d", result);
+        printf("\n");
+    }
+}
+
+int main() {
+    int array[4][2] = { {0} };
+    int choice;
+
+    printf("ROCK - 6 | PAPER - 5 | SCISSORS - 2\n");
+
+    for(int round = 0; round < 4; round++) {
+        printf("\nRound %d:\n", round + 1);
+
+        for(int player_input = 0; player_input < 2; player_input++) {
+            do {
+                printf("Player %d: ", player_input + 1);
+                scanf("%d", &choice);
+
+                if(!(choice == 6 || choice == 5 || choice == 2)) {
+                    printf("\nEnter ROCK - 6, PAPER - 5, or SCISSORS - 2\n");
+                }
+
+            } while(!(choice == 6 || choice == 5 || choice == 2));
+
+            array[round][player_input] = choice;
+        }
+
+        winner(array[round][0], array[round][1]);
+
+        table(array, round);
+    }
+
+    return 0;
+}
